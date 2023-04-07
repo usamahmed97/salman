007be1c6a633ba417a729d295386353c87cc9206
Usama Ahmed Qasmi <usamaahmed97@gmail.com>
2023-04-07 21:00:34 +0500

    Add Prettier, eslint, and framer motion.

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LandingPage = () => {
     return (
-      <div className="text-3xl font-bold underline">
+      <div className="flex items-center justify-center h-screen">
         Landing Page is working
       </div>
     );
